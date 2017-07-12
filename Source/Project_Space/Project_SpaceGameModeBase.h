1a070046368a19023d22e24a7c75b4bd96ee4b82
Roko Milić <cybermen21@hotmail.com>
2017-07-12 02:56:37 +0200

    Proper camera control for ship editor

---
+++ Source/Project_Space/Project_SpaceGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_SpaceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_SPACE_API AProject_SpaceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
